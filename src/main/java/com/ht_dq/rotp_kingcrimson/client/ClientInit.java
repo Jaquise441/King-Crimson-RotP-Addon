be99a9952ef1fddd40a9bf236c1bbf876292135c
sb <sb@sb-HP-Laptop-15s-eq1xxx>
2024-08-25 20:38:05 +0300

    Fixed Stand renderer registration

--- src/main/java/com/ht_dq/rotp_kingcrimson/client/ClientInit.java
+++ src/main/java/com/ht_dq/rotp_kingcrimson/client/ClientInit.java
@@ -1,11 +1,15 @@
 package com.ht_dq.rotp_kingcrimson.client;
 
+import com.ht_dq.rotp_kingcrimson.RotpKingCrimsonAddon;
+import com.ht_dq.rotp_kingcrimson.client.render.entity.renderer.stand.KingCrimsonRenderer;
 import com.ht_dq.rotp_kingcrimson.client.render.vfx.ClientEventHandler2;
 import com.ht_dq.rotp_kingcrimson.client.render.vfx.TemporaryDimensionEffects;
-import com.ht_dq.rotp_kingcrimson.RotpKingCrimsonAddon;
+import com.ht_dq.rotp_kingcrimson.init.AddonStands;
+
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -15,6 +19,7 @@ public class ClientInit {
     @SubscribeEvent
     public static void onFMLClientSetup(FMLClientSetupEvent event) {
         Minecraft mc = Minecraft.getInstance();
+        RenderingRegistry.registerEntityRenderingHandler(AddonStands.KINGCRIMSON.getEntityType(), KingCrimsonRenderer::new);
         event.enqueueWork(() -> {
             ClientEventHandler2.init(mc);
             TemporaryDimensionEffects.init();
