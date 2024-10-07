625b666c7087c0005946fc4c260f5f28ccf9d87c
TheDoGGySL <ilanezenec@gmail.com>
2024-10-07 17:15:11 +0300

    wip (kc user's "time-erase-afterimage"

--- src/main/java/com/ht_dq/rotp_kingcrimson/client/ClientInit.java
+++ src/main/java/com/ht_dq/rotp_kingcrimson/client/ClientInit.java
@@ -5,12 +5,14 @@ import com.ht_dq.rotp_kingcrimson.client.render.entity.renderer.stand.KingCrimso
 import com.ht_dq.rotp_kingcrimson.client.render.vfx.ClientEventHandler2;
 import com.ht_dq.rotp_kingcrimson.client.render.vfx.TemporaryDimensionEffects;
 import com.ht_dq.rotp_kingcrimson.init.AddonStands;
+import com.ht_dq.rotp_kingcrimson.init.InitEntities;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import com.ht_dq.rotp_kingcrimson.client.render.entity.AfterimageRenderer;
 
 @EventBusSubscriber(modid = RotpKingCrimsonAddon.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientInit {
@@ -19,6 +21,7 @@ public class ClientInit {
     public static void onFMLClientSetup(FMLClientSetupEvent event) {
         Minecraft mc = Minecraft.getInstance();
         RenderingRegistry.registerEntityRenderingHandler(AddonStands.KINGCRIMSON.getEntityType(), KingCrimsonRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(InitEntities.AFTERIMAGE.get(), AfterimageRenderer::new);
         event.enqueueWork(() -> {
             ClientEventHandler2.init(mc);
             TemporaryDimensionEffects.init();
--- src/main/java/com/ht_dq/rotp_kingcrimson/init/InitEntities.java
+++ src/main/java/com/ht_dq/rotp_kingcrimson/init/InitEntities.java
@@ -2,6 +2,7 @@ package com.ht_dq.rotp_kingcrimson.init;
 
 import com.github.standobyte.jojo.JojoMod;
 import com.ht_dq.rotp_kingcrimson.RotpKingCrimsonAddon;
+import com.ht_dq.rotp_kingcrimson.entity.AfterimageEntity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,4 +13,7 @@ import net.minecraft.util.ResourceLocation;
 public class InitEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, RotpKingCrimsonAddon.MOD_ID);
 
+    public static final RegistryObject<EntityType<AfterimageEntity>> AFTERIMAGE = ENTITIES.register("afterimage",
+            () -> EntityType.Builder.<AfterimageEntity>of(AfterimageEntity::new, EntityClassification.MISC).sized(0.6F, 1.8F).noSummon().setUpdateInterval(Integer.MAX_VALUE).setShouldReceiveVelocityUpdates(false)
+                    .build(new ResourceLocation(RotpKingCrimsonAddon.MOD_ID, "afterimage").toString()));
 }
