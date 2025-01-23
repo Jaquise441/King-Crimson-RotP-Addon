e3e2bdb2d78a2e2089e05480dc079bac1935f242
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-01-23 04:15:32 +0300

    Made the decoy render as a "projection" entity of the original user

--- src/main/java/com/ht_dq/rotp_kingcrimson/client/render/entity/TimeEraseDecoyRenderer.java
+++ src/main/java/com/ht_dq/rotp_kingcrimson/client/render/entity/TimeEraseDecoyRenderer.java
@@ -4,32 +4,33 @@ import com.ht_dq.rotp_kingcrimson.entity.TimeEraseDecoyEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.entity.BipedRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.client.renderer.entity.LivingRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
 
-// TODO use the stand user's model
-// TODO render layers
-// TODO render name tag
-//public class TimeEraseDecoyRenderer extends EntityRenderer<TimeEraseDecoyEntity> {
-public class TimeEraseDecoyRenderer extends BipedRenderer<TimeEraseDecoyEntity, PlayerModel<TimeEraseDecoyEntity>> {
+public class TimeEraseDecoyRenderer extends EntityRenderer<TimeEraseDecoyEntity> {
 
     public TimeEraseDecoyRenderer(EntityRendererManager renderManager) {
-//        super(renderManager);
-        super(renderManager, new PlayerModel<>(0, false), 0.5F);
+        super(renderManager);
     }
 
-    private static final ResourceLocation DEFAULT_LOCATION = new ResourceLocation("textures/entity/steve.png");
     @Override
     public ResourceLocation getTextureLocation(TimeEraseDecoyEntity p_110775_1_) {
-//        return null;
-        return DEFAULT_LOCATION;
+        return null;
     }
 
     @Override
     public void render(TimeEraseDecoyEntity decoyEntity, float yRotation, float partialTick, MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight) {
-        super.render(decoyEntity, yRotation, partialTick, matrixStack, buffer, packedLight);
+        renderDecoy(decoyEntity.getKCUserToRender(), decoyEntity, yRotation, partialTick, matrixStack, buffer, packedLight);
+    }
+    
+    private <E extends LivingEntity, M extends EntityModel<E>> void renderDecoy(E realEntity, TimeEraseDecoyEntity decoyEntity, float yRotation, float partialTick, MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight) {
+        if (realEntity == null) return;
+        LivingRenderer<E, M> renderer = (LivingRenderer<E, M>) entityRenderDispatcher.getRenderer(realEntity);
+        renderer.render(realEntity, yRotation, partialTick, matrixStack, buffer, packedLight);
     }
 
 }
