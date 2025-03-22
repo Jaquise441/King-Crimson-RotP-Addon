1caebd6743f55a2d256e3f6b2893ccb0a71cd63b
Weever <weever1337@gmail.com>
2025-03-23 00:57:43 +0200

    My way how to do KC Time Skip invulnerable - ❌

--- src/main/java/com/ht_dq/rotp_kingcrimson/action/KingCrimsonTimeSkip.java
+++ src/main/java/com/ht_dq/rotp_kingcrimson/action/KingCrimsonTimeSkip.java
@@ -1,8 +1,5 @@
 package com.ht_dq.rotp_kingcrimson.action;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.action.stand.StandAction;
@@ -12,22 +9,23 @@ import com.ht_dq.rotp_kingcrimson.client.ClientProxy;
 import com.ht_dq.rotp_kingcrimson.config.KCConfig;
 import com.ht_dq.rotp_kingcrimson.init.InitSounds;
 import com.ht_dq.rotp_kingcrimson.util.VFXServerHelper;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.List;
+import java.util.Random;
+
 public class KingCrimsonTimeSkip extends StandAction {
     private static final Random RANDOM = new Random();
-    private static final Map<UUID, Integer> activeTimeSkips = new HashMap<>();
 
     public KingCrimsonTimeSkip(Builder builder) {
         super(builder);
@@ -59,12 +57,10 @@ public class KingCrimsonTimeSkip extends StandAction {
         }
 
         VFXServerHelper.startVFX(user, true);
-
         List<Entity> entities = MCUtil.entitiesAround(LivingEntity.class, user, 32, true, null);
 
         int timeSkipDuration = KCConfig.TIME_SKIP_DURATION.get();
-
-        activeTimeSkips.put(user.getUUID(), timeSkipDuration);
+        user.getTags().add("KingCrimsonTimeSkip");
 
         for (int i = 0; i < timeSkipDuration; i++) {
             for (Entity entity : entities) {
@@ -72,22 +68,20 @@ public class KingCrimsonTimeSkip extends StandAction {
             }
         }
 
-        activeTimeSkips.remove(user.getUUID());
+        user.getTags().remove("KingCrimsonTimeSkip");
 
         userPower.setCooldownTimer(this, KCConfig.TIME_SKIP_COOLDOWN.get());
     }
 
     @Mod.EventBusSubscriber
     public static class EventHandler {
-
         @SubscribeEvent
-        public static void onLivingAttack(LivingAttackEvent event) {
+        public static void onLivingAttack(LivingHurtEvent event) {
             LivingEntity target = event.getEntityLiving();
-            if (target instanceof PlayerEntity) {
+            if (target instanceof PlayerEntity && !target.level.isClientSide) {
                 PlayerEntity player = (PlayerEntity) target;
-                UUID playerId = player.getUUID();
 
-                if (activeTimeSkips.containsKey(playerId)) {
+                if (player.getTags().contains("KingCrimsonTimeSkip")) {
                     event.setCanceled(true);
                 }
             }
@@ -96,11 +90,9 @@ public class KingCrimsonTimeSkip extends StandAction {
         @SubscribeEvent
         public static void onLivingFall(LivingFallEvent event) {
             LivingEntity target = event.getEntityLiving();
-            if (target instanceof PlayerEntity) {
+            if (target instanceof PlayerEntity && !target.level.isClientSide) {
                 PlayerEntity player = (PlayerEntity) target;
-                UUID playerId = player.getUUID();
-
-                if (activeTimeSkips.containsKey(playerId)) {
+                if (player.getTags().contains("KingCrimsonTimeSkip")) {
                     event.setCanceled(true);
                 }
             }
