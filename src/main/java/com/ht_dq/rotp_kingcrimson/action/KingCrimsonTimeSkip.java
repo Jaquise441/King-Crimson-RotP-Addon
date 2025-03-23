0442d5f4c48519cdc0fcdf151e13e44e1a879915
Yarost228 <yarik.alferov1@gmail.com>
2025-03-23 20:53:47 +0300

    Removed damage in Timeskip

--- src/main/java/com/ht_dq/rotp_kingcrimson/action/KingCrimsonTimeSkip.java
+++ src/main/java/com/ht_dq/rotp_kingcrimson/action/KingCrimsonTimeSkip.java
@@ -2,9 +2,14 @@ package com.ht_dq.rotp_kingcrimson.action;
 
 import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.action.ActionTarget;
+import com.github.standobyte.jojo.action.player.ContinuousActionInstance;
+import com.github.standobyte.jojo.action.player.IPlayerAction;
 import com.github.standobyte.jojo.action.stand.StandAction;
+import com.github.standobyte.jojo.capability.entity.PlayerUtilCap;
+import com.github.standobyte.jojo.power.impl.nonstand.INonStandPower;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.github.standobyte.jojo.util.mc.MCUtil;
+import com.github.standobyte.jojo.util.mc.damage.DamageUtil;
 import com.ht_dq.rotp_kingcrimson.client.ClientProxy;
 import com.ht_dq.rotp_kingcrimson.config.KCConfig;
 import com.ht_dq.rotp_kingcrimson.init.InitSounds;
@@ -12,6 +17,7 @@ import com.ht_dq.rotp_kingcrimson.util.VFXServerHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -24,7 +30,7 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.List;
 import java.util.Random;
 
-public class KingCrimsonTimeSkip extends StandAction {
+public class KingCrimsonTimeSkip extends StandAction implements IPlayerAction<KingCrimsonTimeSkip.Instance, IStandPower> {
     private static final Random RANDOM = new Random();
 
     public KingCrimsonTimeSkip(Builder builder) {
@@ -57,10 +63,10 @@ public class KingCrimsonTimeSkip extends StandAction {
         }
 
         VFXServerHelper.startVFX(user, true);
+        setPlayerAction(user, userPower);
         List<Entity> entities = MCUtil.entitiesAround(LivingEntity.class, user, 32, true, null);
 
         int timeSkipDuration = KCConfig.TIME_SKIP_DURATION.get();
-        user.getTags().add("KingCrimsonTimeSkip");
 
         for (int i = 0; i < timeSkipDuration; i++) {
             for (Entity entity : entities) {
@@ -68,34 +74,37 @@ public class KingCrimsonTimeSkip extends StandAction {
             }
         }
 
-        user.getTags().remove("KingCrimsonTimeSkip");
-
         userPower.setCooldownTimer(this, KCConfig.TIME_SKIP_COOLDOWN.get());
     }
 
-    @Mod.EventBusSubscriber
-    public static class EventHandler {
-        @SubscribeEvent
-        public static void onLivingAttack(LivingHurtEvent event) {
-            LivingEntity target = event.getEntityLiving();
-            if (target instanceof PlayerEntity && !target.level.isClientSide) {
-                PlayerEntity player = (PlayerEntity) target;
+    @Override
+    public Instance createContinuousActionInstance(LivingEntity user, PlayerUtilCap userCap, IStandPower power) {
+        return new Instance(user, userCap, power, this);
+    }
 
-                if (player.getTags().contains("KingCrimsonTimeSkip")) {
-                    event.setCanceled(true);
+    public static class Instance extends ContinuousActionInstance<KingCrimsonTimeSkip, IStandPower> {
+
+        public Instance(LivingEntity user, PlayerUtilCap userCap,
+                        IStandPower playerPower, KingCrimsonTimeSkip action) {
+            super(user, userCap, playerPower, action);
+        }
+
+        private int invTicks = 10;
+
+        @Override
+        public boolean cancelIncomingDamage(DamageSource dmgSource, float dmgAmount) {
+            return true;
+        }
+        @Override
+        public void playerTick() {
+            LivingEntity user = getUser();
+            if (!user.level.isClientSide()) {
+                invTicks--;
+                if (invTicks <= 0){
+                    stopAction();
                 }
             }
         }
 
-        @SubscribeEvent
-        public static void onLivingFall(LivingFallEvent event) {
-            LivingEntity target = event.getEntityLiving();
-            if (target instanceof PlayerEntity && !target.level.isClientSide) {
-                PlayerEntity player = (PlayerEntity) target;
-                if (player.getTags().contains("KingCrimsonTimeSkip")) {
-                    event.setCanceled(true);
-                }
-            }
-        }
     }
 }
